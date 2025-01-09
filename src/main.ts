abb3872094a08fc1b4fc03ca2ee712b40b57a3f2
lgcjh0s <redballs@naver.com>
2025-01-10 08:03:49 +0900

    c

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { Logger } from './common/common.logger';
+import Handlebars from 'handlebars';
 
 async function bootstrap() {
 
@@ -11,6 +12,9 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setBaseViewsDir(join(__dirname, '..', 'service'));
   app.setViewEngine('hbs');
+
+  Handlebars.registerHelper('comma', (num: number) => num.toLocaleString());
+
   await app.listen(process.env.PORT ?? 3000);
 
   logger.info('Server started!');
