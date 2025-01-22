48c7cb29dc993501be7347220b6ed3d99bccf694
lgcjh0s <redballs@naver.com>
2025-01-23 07:58:32 +0900

    c

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService extends BaseService {
             sub: user.userId
         };
         return {
-            access_token: this.jwtService.sign(payload)
+            access_token: this.jwtService.sign(payload, { secret: 'test0101!!' })
         }
     }
 
